0c1cd017cc40b8bf906da36d56654bf87cadfdb7
Gilad Ronat <me@giladronat.com>
2019-07-31 15:43:50 -0700

    Stub play with playback media

--- NineAnimator/Controllers/Custom Player Scene/CustomPlayerViewController.swift
+++ NineAnimator/Controllers/Custom Player Scene/CustomPlayerViewController.swift
@@ -10,19 +10,47 @@ import AVFoundation
 import UIKit
 
 class CustomPlayerViewController: UIViewController {
-
+    private var media: PlaybackMedia?
+    private var playerItem: AVPlayerItem? {
+        return media?.avPlayerItem
+    }
+    
     private let player = AVPlayer()
     
-    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet private weak var playerView: PlayerView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    func play(_ media: PlaybackMedia) {
+        self.media = media
         preparePlayer()
+        play()
     }
     
     private func preparePlayer() {
+        // Set up view layer
         playerView.player = player
+        
+        if let playerItem = playerItem {
+            addPlayerItemObservers(playerItem)
+        }
+        addPlayerObservers(player)
+        
+        // Set up player with item
+        player.replaceCurrentItem(with: playerItem)
     }
-
+    
+    private func addPlayerItemObservers(_ playerItem: AVPlayerItem) {
+        //
+    }
+    
+    private func addPlayerObservers(_ player: AVPlayer) {
+        //
+    }
+    
+    private func play() {
+        player.play()
+    }
+    
+    private func pause() {
+        player.pause()
+    }
 }
